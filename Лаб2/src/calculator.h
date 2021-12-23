ee972c465a97bd371dc4e1bba3bf4728f6f123ff
LekhaKhan <68515679+LekhaKhan@users.noreply.github.com>
2021-12-24 00:34:05 +0200

    Add files via upload

---
+++ Лаб2/src/calculator.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+
+class calculator
+{
+    public:
+        int Add (double, double);
+        int Mul (double, double);
+        int Sub(double, double);
+};
+
+#endif//CALCULATOR_H
